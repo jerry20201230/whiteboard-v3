add27f9e147869b0ca65c4897810013762e75f2e
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2022-11-27 17:00:01 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/lib',(req,res)=>{
+    res.sendFile(__dirname+__filename)
+})
+app.get('*',(req,res)=>{
+    res.status(404).sendFile(__dirname+'/lib/404.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    
+    socket.on("CanvasUpdate",(e)=>{
+     
+      socket.broadcast.emit("CanvasUpdate",e)
+    })
+  });
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
