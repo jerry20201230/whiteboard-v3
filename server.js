25c541fab33987d6fcb5eb30215a6c9eb18cb0ef
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2022-12-18 09:01:32 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,37 +4,92 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+app.get(/js|css|html/, (req, res) => {
+  res.sendFile(__dirname + __filename)
+})
+app.get('/lib/health', (req, res) => {
+  res.sendStatus(200)
+})
+app.get('*', (req, res) => {
+  res.status(404).sendFile(__dirname + '/lib/404.html')
+})
 
-app.get("/lib",(req,res)=>{
-    res.sendFile(__dirname+__filename)
-})
-app.get('/lib/health',(req,res)=>{
-    res.sendStatus(200)
-})
-app.get('*',(req,res)=>{
-    res.status(404).sendFile(__dirname+'/lib/404.html')
-})
+var user = {
+  IdList : ["@not_logged_in_user", "@admin"],
+  PassList :  ["", "tgedqa123"],
+  TakenList : [101010,0]
+}
+
+
+
+var chatId = 0
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  console.log('a user connected');
+  socket.emit("data-update", { chat_id: chatId })
 
-    
-    socket.on("CanvasUpdate",(e)=>{
-     
-      socket.broadcast.emit("CanvasUpdate",e)
-    })
-    socket.on("ChatMsg",(e)=>{
-      socket.broadcast.emit("ChatMsg",e);
-      console.log(e)
-    })
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+
+
+  socket.on("CanvasUpdate", (e) => {
+
+    socket.broadcast.emit("CanvasUpdate", e)
+  })
+  socket.on("ChatMsg", (e) => {
+    socket.broadcast.emit("ChatMsg", e);
+    console.log(e)
+    chatId++
+  })
+
+  socket.on("loginData", (e) => {
+    console.log(e)
+    console.log(socket.id)
+
+    var status = false
+    var random = getRandomInt(100000,999999);
+    var userIndex;
+    for (i = 0; i < user.IdList.length; i++) {
+
+      if (e.un === user.IdList[i] && e.up === user.PassList[i]) {
+        status = true;
+        userIndex = i;
+        break;
+      }
+    }
+   
+    while(user.TakenList.includes(random)){
+      random = getRandomInt(100000,999999);
+    }
+
+    user.TakenList[userIndex] = random
+    if (status === true) {
+      io.to(socket.id).emit("loginStatus", { 'status': 'success', 'un': e.un, 'up': e.up ,'tk':random})
+    }
+    else {
+      io.to(socket.id).emit("loginStatus", { 'status': 'no_match_data', 'un': e.un, 'up': e.up,'tk':random })
+    }
+
+
+  })
+});
+server.listen(port, () => {
+  console.log(`running on ${__dirname}:${port}`);
 });
