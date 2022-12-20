2692bab9376a9b3b9a3096982cd08883620eceec
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2022-12-20 19:18:16 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -26,6 +26,37 @@ app.get('*', (req, res) => {
   res.status(404).sendFile(__dirname + '/lib/404.html')
 })
 
+
+
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
 var user = {
   IdList : ["@not_logged_in_user", "@admin"],
   PassList :  ["", "tgedqa123"],
