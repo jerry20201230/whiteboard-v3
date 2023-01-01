ddae30335781552981d55920857575474ed6d93e
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-01-01 16:30:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 3000;
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const session = require('express-session');
 const path = require("path")
 
